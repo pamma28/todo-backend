0cc863b1dbd5779ca5af7d5d2de57b7dcd1d050b
Yunas Pamatda <Yunas.Pamatda@mitrais.com>
2020-05-13 09:17:40 +0700

    add body parser on app

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const Env = require('./helpers/environment');
 const todosRoute = require('./routes/todo.routes');
 const app = express();
 const port = process.env.PORT || Env.get('APP_PORT');
 
-app.get('/', (req, res) => res.send('Todos Backend Server, access swagger for more detail.'));
+app.use(bodyParser.json());
+app.get('/', (req, res) =>
+  res.send('Todos Backend Server, access swagger for more detail.')
+);
 app.use(todosRoute);
 
 app.listen(port, () =>
