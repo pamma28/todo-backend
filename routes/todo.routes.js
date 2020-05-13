6896934b03147ba97f661981aebff5f28615e0bf
Yunas Pamatda <Yunas.Pamatda@mitrais.com>
2020-05-13 09:36:50 +0700

    Revert "add config mongodb for database"
    This reverts commit fe1796b85a8508e7d8c09e759a14590de1c76803.

--- routes/todo.routes.js
+++ routes/todo.routes.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const todosRoutes = new express.Router();
-const { addTodo, getTodos } = require('../controllers/todos.controller');
 
-// get todos
+// get
 todosRoutes.route('/todos').get(async (req, res) => {
-  await getTodos(req, res);
-});
-// add todos
-todosRoutes.route('/todos').post(async (req, res) => {
-  await addTodo(req, res);
+  try {
+    res.send([]);
+  } catch (error) {
+    res.send({ error: true, message: error.message });
+  }
 });
 
 module.exports = todosRoutes;
