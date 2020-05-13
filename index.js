4b3005d0e9dd09dbf354457b82c7b9ec9b0dd124
Yunas Pamatda <Yunas.Pamatda@mitrais.com>
2020-05-13 11:26:29 +0700

    add swagger docs for each end point

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const swaggerUi = require('swagger-ui-express');
+
 const Env = require('./helpers/environment');
 const todosRoute = require('./routes/todo.routes');
+const swaggerDocument = require('./swagger.json');
+
 const app = express();
 const port = process.env.PORT || Env.get('APP_PORT');
 
@@ -10,6 +14,7 @@ app.get('/', (req, res) =>
   res.send('Todos Backend Server, access swagger for more detail.')
 );
 app.use(todosRoute);
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(port, () =>
   console.log(`app listening at http://localhost:${port}`)
